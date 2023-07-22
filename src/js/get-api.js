fb7c1d4efaab643b44b01df5b0213006b7786278
ViktoriiaNeroda <viktoriaaaneroda@gmail.com>
2023-07-22 19:37:52 +0300

    оновлено АРІ

--- src/js/get-api.js
+++ src/js/get-api.js
@@ -4,23 +4,23 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '38392078-db89c19ac20a7bd4eddb18d45';
 
-export async function getAsked(inputedData, pageNumber, perPage) {
-  const options = new URLSearchParams({
-    key: `${API_KEY}`,
-    q: `${inputedData}`,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-    page: pageNumber,
-    per_page: perPage,
-  });
+export async function getAsked(inputedData, pageNumber = 1, perPage = 10) {
   try {
-    return await axios.get(`${BASE_URL}?${options}`);
+    const options = new URLSearchParams({
+      key: API_KEY,
+      q: encodeURIComponent(inputedData),
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      page: pageNumber,
+      per_page: perPage,
+    });
+
+    const response = await axios.get(`${BASE_URL}?${options}`);
+    return response;
   } catch (error) {
-    Notify.info(
-      'Sorry.',
-      'There are no images matching your search query. Please try again.',
-      'Okay'
-    );
+    Notify.failure('Oops!', 'Something went wrong. Please try again later.');
+   
+    throw error; 
   }
 }
